f90ad646ce5e095a853765dc84c123763eea8661
limeng <limengjch@163.com>
2021-02-24 21:15:38 +0800

    ADD：初始化项目

---
+++ src/main/java/com/magic/nes/NesApplication.java
@@ -0,0 +1,13 @@
+package com.magic.nes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NesApplication.class, args);
+    }
+
+}
